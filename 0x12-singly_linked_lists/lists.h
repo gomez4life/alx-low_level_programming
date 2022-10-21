350f1f9fb3fba8b9024eafe7d8a4f46637691371
Gomina Peter <111024729+gomez4life@users.noreply.github.com>
2022-10-21 15:43:49 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,30 +1,36 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LISTS_H
+#define LISTS_H
 
+/* include header file for output fuctions like printf */
 #include <stdio.h>
-#include <stdlib.h>
+/* incluse header file for string manipulation */
 #include <string.h>
+/* include header file for memory allocation */
+#include <stdlib.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct list_s - singly linked lists
+ * @str: malloc'ed string
+ * @len: length of string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
-
+/* print all elements of linked list; return number of nodes */
 size_t print_list(const list_t *h);
+/* count number of elements of linked list return the amount */
 size_t list_len(const list_t *h);
+/* add elements to the beginning of the list; return address of element */
 list_t *add_node(list_t **head, const char *str);
+/* add elements to the end of the list; return address of element */
 list_t *add_node_end(list_t **head, const char *str);
+/* free a list_t list by calling free */
 void free_list(list_t *head);
 
-#endif
+#endif /* LISTS_H*/
